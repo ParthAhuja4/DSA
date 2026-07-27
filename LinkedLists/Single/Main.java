ddcd6edd2409fba56d8a2a5198ee71b95de0d2b4
Parth Ahuja <parthahuja006@gmail.com>
2026-07-27 16:48:16 +0530

    Single LL

--- LinkedLists/Single/Main.java
+++ LinkedLists/Single/Main.java
@@ -2,12 +2,20 @@ package LinkedLists.Single;
 
 public class Main {
     public static void main(String[] args) {
-        LL linkedList = new LL();
+        SingleLL linkedList = new SingleLL();
         linkedList.insertFirst(9);
         linkedList.insertFirst(7);
         linkedList.insertFirst(5);
         linkedList.insertLast(11);
         linkedList.insertAt(2, -1);
+        linkedList.display();
+        linkedList.deleteFirst();
+        linkedList.deleteAt(1);
+        linkedList.display();
+        linkedList.deleteLast();
+        linkedList.display();
+        System.out.println(linkedList.get(1));
+        linkedList.reverse();
         linkedList.display();
     }
 }
---
+++ LinkedLists/Single/SingleLL.java
@@ -0,0 +1,145 @@
+package LinkedLists.Single;
+
+public class SingleLL {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(this.value);
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public SingleLL() {
+        this.size = 0;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid position");
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public void insertAt(int index, int value) throws IndexOutOfBoundsException {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid position");
+        }
+        if (index == 0) {
+            insertFirst(value);
+            return;
+        }
+        if (index == this.size) {
+            insertLast(value);
+            return;
+        }
+
+        Node prev = this.get(index - 1);
+
+        prev.next = new Node(value, prev.next);
+        this.size++;
+    }
+
+    public void insertFirst(int value) {
+        Node node = new Node(value, head);
+        this.head = node;
+        if (tail == null) {
+            tail = head;
+        }
+        this.size++;
+    }
+
+    public void insertLast(int value) {
+        if (tail == null) {
+            insertFirst(value);
+            return;
+        }
+        Node node = new Node(value, null);
+        tail.next = node;
+        this.tail = node;
+        this.size++;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+    public void deleteFirst() {
+        this.head = head.next;
+        if (head == null) {
+            this.tail = null;
+        }
+        this.size--;
+    }
+
+    public void deleteLast() {
+        if (size <= 1) {
+            deleteFirst();
+            return;
+        }
+        Node scndLast = this.get(size - 2);
+        this.tail = scndLast;
+        scndLast.next = null;
+        this.size--;
+    }
+
+    public void deleteAt(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid position");
+        }
+        if (index == 0) {
+            deleteFirst();
+            return;
+        }
+        if (index == size - 1) {
+            deleteLast();
+            return;
+        }
+
+        Node prev = this.get(index - 1);
+        Node toDltNode = prev.next;
+        prev.next = prev.next.next;
+        toDltNode.next = null;
+        this.size--;
+    }
+
+    public void reverse() {
+        if (this.head == null) {
+            throw new IndexOutOfBoundsException("Empty LL");
+        }
+        if (this.size == 1) {
+            return;
+        }
+        Node currNode = this.head;
+        Node prevNode = null;
+
+        while (currNode != null) {
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head = prevNode;
+    }
+
+}
